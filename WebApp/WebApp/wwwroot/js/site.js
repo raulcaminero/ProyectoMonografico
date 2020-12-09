81f91603fec777ff488f409b90b09d9b57cb8985
Zack <ZacarysHeredia@gmail.com>
2020-12-08 21:07:30 -0400

    DataTable with filter implemented in Carreras

--- WebApp/WebApp/wwwroot/js/site.js
+++ WebApp/WebApp/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    var careersTB = $("#careers-tb").DataTable();
+
+    yadcf.init(careersTB,
+        [
+            {
+                column_number: 2,
+                filter_type: "select",
+                filter_default_label: "Todas"
+            },
+            {
+                column_number: 5,
+                filter_type: "select",
+                filter_default_label: "Todas",
+                data: ["Activo", "Inactivo"]
+            }
+        ],
+        {
+            cumulative_filtering: true
+        }
+    );
+
+    $(".yadcf-filter-reset-button").remove();
+});
