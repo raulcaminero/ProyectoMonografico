292fd65fcc7e24477f102d5bb99977c688bc1691
Willkelly Alcántara Castillo <w.alcantara@tc.gob.do>
2020-12-19 19:48:54 -0400

    a

---
+++ WebApp/WebApp/Models/GeneralPurpose.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class GeneralPurpose
+    {
+        public GeneralPurpose() { 
+        
+        }
+
+        public string GetUserPicture(string usr)
+        {
+            return "https://tribunalsitestorage.blob.core.windows.net/media/24005/whatsapp-image-2020-11-27-at-93025-pm.jpeg";
+        }
+    }
+}
